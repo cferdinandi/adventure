5e0b2a41d35ad1f9983748ce8f875d0326949ee0
cferdinandi <chris@gomakethings.com>
2024-08-07 23:43:30 -0400

    Updated content

--- themes/gmt/src/js/print.js
+++
@@ -1,8 +0,0 @@
-var details = Array.prototype.slice.call(document.querySelectorAll('details'));
-if (details.length) {
-	window.addEventListener('beforeprint', function () {
-		details.forEach(function (detail) {
-			detail.setAttribute('open', '');
-		});
-	});
-}
--- themes/gmt/src/js/tableOfContents.js
+++
@@ -1,7 +0,0 @@
-import tableOfContents from '../../../../../gmt-theme/dist/js/tableOfContents.js';
-
-// Add table of contents
-var toc = document.querySelector('[data-toc]');
-if (toc) {
-	tableOfContents('[data-toc]', 'h2', '<h2>' + toc.getAttribute('data-toc') + '</h2>', null, 'ol');
-}
